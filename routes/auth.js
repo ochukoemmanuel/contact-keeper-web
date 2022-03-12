c712cff94afde9b578146408922db8bdff6fc5e5
Emmy <ochukoemmanuel07@gmail.com>
2022-03-12 23:30:00 +0100

    Authenticate Route/Login

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,72 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
+const bcrypt = require("bcryptjs/dist/bcrypt");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator/check");
+
+const User = require("../models/Users");
 
 // @route     GET api/auth
-// @desc      Get logged in user 
+// @desc      Get logged in user
 // @access    Private
-router.get('/', (req, res) =>  {
-    res.send('Get logged in user');
-})
+router.get(
+  "/",
+  [
+    check("email", "Please include a valiid email").isEmail(),
+    check("password", "Password is required").isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ msg: "Invalid Credentials" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({msg: 'Invalid Credentials'});
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (e) {
+      console.error(e.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 // @route     POST api/auth
-// @desc      Auth user & get token 
+// @desc      Auth user & get token
 // @access    Private
-router.get('/', (req, res) =>  {
-    res.send('Log in user');
-})
+router.get("/", (req, res) => {
+  res.send("Log in user");
+});
 
 module.exports = router;
