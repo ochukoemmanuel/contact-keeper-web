d3aa17a4d5bd906a746fbd51b33b764d719f9e5a
Emmy <ochukoemmanuel07@gmail.com>
2022-03-12 23:06:52 +0100

    create and repond with json web token

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const bcrypt = require("bcryptjs/dist/bcrypt");
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator/check");
 
 const User = require("../models/Users");
@@ -45,7 +47,23 @@ router.post(
 
       await user.save();
 
-      res.send("User save");
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (e) {
       console.error(e.message);
       res.status(500).send("Server Error");
