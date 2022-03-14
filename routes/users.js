d739aeacc5ecf1a2bc73dbae66a237ebfcb46e69
Emmy <ochukoemmanuel07@gmail.com>
2022-03-15 00:57:06 +0100

    created contact model & post, get route, created new react app. setup react concurrently

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const config = require("config");
-const { check, validationResult } = require("express-validator/check");
+const { check, validationResult } = require("express-validator");
 
 const User = require("../models/Users");
 
