0f9e93121a3813b6f1b4ca277d769ab4c2240bb9
Emmy <ochukoemmanuel07@gmail.com>
2022-03-14 09:35:28 +0100

    Auth middleware and protecting routes

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const bcrypt = require("bcryptjs/dist/bcrypt");
 const jwt = require("jsonwebtoken");
 const config = require("config");
+const auth = require("../middleware/auth");
 const { check, validationResult } = require("express-validator/check");
 
 const User = require("../models/Users");
@@ -11,7 +12,20 @@ const User = require("../models/Users");
 // @route     GET api/auth
 // @desc      Get logged in user
 // @access    Private
-router.get(
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user)
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error')
+  }
+})
+
+// @route     POST api/auth
+// @desc      Auth user $get token
+// @access    Private
+router.post(
   "/",
   [
     check("email", "Please include a valiid email").isEmail(),
