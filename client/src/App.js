7d31cbfcb928c3863d9f455fe7f180fbec5de70e
Emmy <ochukoemmanuel07@gmail.com>
2022-03-15 01:41:48 +0100

    Created Navbar and Router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import "./App.css";
+import Home from "./components/layout/Home";
+import About from "./components/layout/About";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment className="App">
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </Fragment>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/layout/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this App</h1>
+      <p className="my-1">Full stack react App for keeping contact</p>
+      <p className="bg-dark p">
+        <strong>Version</strong> 1.1
+      </p>
+    </div>
+  );
+};
+
+export default About;
